5946d4fcde3d49bc27911845d9097d5ed6a6063b
Rahzae Williams <84481655+Rahzae@users.noreply.github.com>
2022-05-16 17:57:37 -0400

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCA6HyIjiUmZAC3oZrY1BnphaxXvVOgKJM",
+    authDomain: "whatsapp-3b452.firebaseapp.com",
+    projectId: "whatsapp-3b452",
+    storageBucket: "whatsapp-3b452.appspot.com",
+    messagingSenderId: "301742129009",
+    appId: "1:301742129009:web:581d3bb8449214c2a880d0"
+  };
